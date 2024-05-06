e9f9708c87247c079abd7c738d79dc72ea31a9c5
felipediaz3974@gmail.com <felipediaz3974@gmail.com>
2024-05-06 16:15:31 -0300

    dynamic menu

--- menu.js
+++ menu.js
@@ -1,17 +1,23 @@
-
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
-
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
-
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
-
+document.addEventListener('DOMContentLoaded', function () {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            
+            if (targetElement) {
+                const offset = targetElement.getBoundingClientRect().top + window.pageYOffset;
+                const scrollOptions = {
+                    top: offset,
+                    behavior: 'smooth', 
+                    duration: 1000 
+                };
+                
+                window.scrollTo(scrollOptions);
+            }
+        });
+    });
+});
